aad110188fd4d4fac6db4b5fb3b9043694638e60
Klaus Zerwes <zerwes@users.noreply.github.com>
2014-11-24 13:21:17 +0100

    fix print for py3

--- examples/odyldocompare.py
+++ examples/odyldocompare.py
@@ -14,22 +14,22 @@ if not FILES:
 	FILES = ['test/odyl.yaml']
 
 for F in FILES:
-	print '='*60
+	print ('='*60)
 
-	print '-'*10, ' ORIGINAL %s ' % F, '-'*10
+	print ('-'*10, ' ORIGINAL %s ' % F, '-'*10)
 	f = open(F, 'r')
 	ydata = yaml.safe_load(f)
 	print(ydata)
-	print '-'*60
+	print ('-'*60)
 	print (yaml.safe_dump(ydata, default_flow_style = False))
 
-	print '='*60
+	print ('='*60)
 
-	print '-'*10, ' ODYLDo %s ' % F, '-'*10
+	print ('-'*10, ' ODYLDo %s ' % F, '-'*10)
 	f = open(F, 'r')
 	ydata = odyldo.safe_load(f)
 	print(ydata)
-	print '-'*60
+	print ('-'*60)
 	print(odyldo.safe_dump(ydata, default_flow_style = False))
-	print '='*60
+	print ('='*60)
 
