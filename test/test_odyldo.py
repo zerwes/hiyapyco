2702d0d6ebc6c4069350da9b17968a2871000d4b
Klaus Zerwes <zerwes@users.noreply.github.com>
2014-11-24 12:33:31 +0100

    test script

--- test/test_odyldo.py
+++ test/test_odyldo.py
@@ -3,10 +3,25 @@ import yaml
 import sys
 import os
 
-f = open('odyl.yaml', 'r')
-ydata = yaml.load(f, odyldo.ODYL)
 
-print ydata
 
+
+print '='*60
+
+print '-'*10, ' ORIGINAL ', '-'*10
+f = open('test/odyl.yaml', 'r')
+ydata = yaml.safe_load(f)
+print(ydata)
+print '-'*60
 print (yaml.safe_dump(ydata, default_flow_style = False))
+
+print '='*60
+
+print '-'*10, ' ODYLDo ', '-'*10
+f = open('test/odyl.yaml', 'r')
+ydata = odyldo.safe_load(f)
+print(ydata)
+print '-'*60
+print(odyldo.safe_dump(ydata, default_flow_style = False))
+print '='*60
 
