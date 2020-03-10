0aac53d807e560adb639b729f7ff9608dec0e417
Klaus Zerwes <zerwes@users.noreply.github.com>
2020-03-10 09:35:49 +0100

    extendet tests for PR #37

--- test/test_merge.py
+++ test/test_merge.py
@@ -29,6 +29,54 @@ print('start test %s for hiyapyco %s using python %s (loglevel:%s)' % (
         )
     )
 
+logger.info('test substitute vals ...')
+conf = hiyapyco.load(
+        os.path.join(basepath, 'base.yaml'),
+        os.path.join(basepath, 'baseext.yaml'),
+        method=hiyapyco.METHOD_SUBSTITUTE,
+        failonmissingfiles=True
+        )
+
+t = conf['singel']
+logger.info('test single val ... %s' % t)
+assert t == 'ext'
+
+t = conf['int']
+logger.info('test int val ... %s' % t)
+assert t == 10
+
+t = conf['array']
+logger.info('test list val ... %s' % t)
+assert t == ['base1', 'base2', 'baseext1', 'baseext2']
+
+t = conf['hash']
+logger.info('test simple dict ... %s' % t)
+assert t == {'k1': 'b1', 'k2': 'b2', 'ek1': 'be1', 'ek2': 'be2'}
+
+t = conf['deeplist']
+logger.info('test deeplist ... %s' % t)
+assert t == [{'d1': {'d1k1': 'v1', 'd1k2': 'v2', 'd1ext': 'vext'}}, {'d2': {'d2k2': 'x2', 'd2k1': 'x1'}}, {'d32': {'a': 'A2A2', 'c': 'CCC', 'b': 'B2'}, 'd31': {'a': 'AAA', 'c': 'C', 'b': 'B'}}, {'dext1': {'d1k1ext': 'v1ext'}}]
+
+t = conf.get('deepmap')
+logger.info('test deepmap ... %s' % t)
+assert t == {'l1k1' : {'l2k1': 'overwr', 'l2k2': 'abc', 'l2k3': 'new val'}, 'l1k2': {'l2k1': 'bli', 'l2k2': 'bla', 'l2k3': 'blub'}, 'l1k1ext': {'l2k1ext': 'ext'}}
+
+t = conf['ext']
+logger.info('test extonly ... %s' % t)
+assert t == 'extendet only'
+
+t = conf['missing_key']
+logger.info('test missing_key ... %s' % t)
+assert t == 'one'
+
+t = conf['missing_key_parent']
+logger.info('test missing_key_parent ... %s' % t)
+assert t == {'a': 'b'}
+
+t = conf['common_key']
+logger.info('test common_key ... %s' % t)
+assert t == {'common_subkey_deep': 'oneext', 'missing_key_base': 'val2', 'missing_key_ext': 'val2missingInBase'}
+
 logger.info('test simple vals ...')
 conf = hiyapyco.load(
         os.path.join(basepath, 'base.yaml'),
@@ -128,4 +176,3 @@ except KeyError as e:
 print('passed test %s' % __file__)
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent nu
-
