8a340837103553ae83e51d87dd986f36a6e4937a
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2024-10-18 06:19:29 +0000

    apply suggestions from code review

--- hiyapyco/__init__.py
+++ hiyapyco/__init__.py
@@ -350,23 +350,14 @@ class HiYaPyCo:
         if self.dereferenceyamlanchors:
             a = copy.deepcopy(a)
             b = copy.deepcopy(b)
-        logger.debug(
-            "simplemerge %s (%s) and %s (%s)"
-            % (
-                a,
-                type(a),
-                b,
-                type(b),
-            )
-        )
+        logger.debug('simplemerge %s (%s) and %s (%s)' % (a, type(a), b, type(b),))
         if b is None:
-            logger.debug('pass as b is None')
             # override -> None replaces object, no matter what.
             if self.none_behavior == NONE_BEHAVIOR_OVERRIDE:
+                logger.debug('b is None + none_behavior in use => return None')
                 return None
             # default behavior is to attempt merge or fail
-            else:
-                pass
+            logger.debug('pass as b is None')
         elif isinstance(b, primitiveTypes):
             logger.debug('simplemerge: primitiveTypes replace a "%s"  w/ b "%s"' % (a, b,))
             a = b
@@ -401,21 +392,15 @@ class HiYaPyCo:
             a = copy.deepcopy(a)
             b = copy.deepcopy(b)
         logger.debug('>' * 30)
-        logger.debug(
-            "substmerge %s and %s"
-            % (
-                a,
-                b,
-            )
-        )
+        logger.debug('substmerge %s and %s' % (a, b,))
+        # FIXME: make None usage configurable
         if b is None:
             logger.debug('pass as b is None')
             # override -> None replaces object, no matter what.
             if self.none_behavior == NONE_BEHAVIOR_OVERRIDE:
+                logger.debug('b is None + none_behavior in use => return None')
                 return None
             # default behavior is to attempt merge or fail
-            else:
-                pass
 
         # treat listTypes as primitiveTypes in merge
         # subsititues list, don't merge them
@@ -466,13 +451,8 @@ class HiYaPyCo:
         if self.dereferenceyamlanchors:
             a = copy.deepcopy(a)
             b = copy.deepcopy(b)
-        logger.debug(
-            "deepmerge %s and %s"
-            % (
-                a,
-                b,
-            )
-        )
+        logger.debug('deepmerge %s and %s' % (a, b,))
+        # FIXME: make None usage configurable
         if b is None:
             logger.debug('pass as b is None')
             # override -> None replaces object, no matter what.
