4397fe7eed06a8bccf4aa30cbcc378d097157d76
Klaus Zerwes <zerwes@users.noreply.github.com>
2024-10-19 06:36:56 +0200

    moved test_ruamel_yaml_loader.py to examples

--- examples/test_ruamel_yaml_loader.py
+++ examples/test_ruamel_yaml_loader.py
@@ -15,7 +15,7 @@ import hiyapyco
 sys.path.insert(
     0,
     os.path.join(
-        os.path.dirname(os.path.realpath(os.path.abspath(sys.argv[0]))), "lib"
+        os.path.dirname(os.path.realpath(os.path.abspath(sys.argv[0]))), "../test/lib"
     ),
 )
 import testsetup
@@ -42,7 +42,7 @@ def ruamel_loader(stream):
     return yaml.load_all(stream)
 
 
-conf = hiyapyco.load(os.path.join(basepath, "base.yaml"), loader_callback=ruamel_loader)
+conf = hiyapyco.load(os.path.join(basepath, "../test/base.yaml"), loader_callback=ruamel_loader)
 
 expected = {'singel': 'base', 'int': 1, 'array': ['base1', 'base2'], 'hash': {'k1': 'b1', 'k2': 'b2'}, 'deeplist': [{'d1': {'d1k1': 'v1', 'd1k2': 'v2'}}, {'d2': {'d2k1': 'x1', 'd2k2': 'x2'}}, {'d31': {'a': 'A', 'b': 'B', 'c': 'C'}, 'd32': {'a': 'A2', 'b': 'B2'}}], 'deepmap': {'l1k1': {'l2k1': 'xyz', 'l2k2': 'abc'}, 'l1k2': {'l2k1': 'bli', 'l2k2': 'bla', 'l2k3': 'blub'}}, 'missing_key': 'one', 'common_key': {'common_subkey_deep': 'one', 'missing_key_base': 'val2'}}
 
