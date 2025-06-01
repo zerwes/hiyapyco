f27689b57b8e1b341ffb8d19b5f38c41e2d79f9c
Klaus Zerwes <zerwes@users.noreply.github.com>
2025-06-01 10:29:23 +0200

    test for pr #84 using NONE_BEHAVIOR_IGNORE

---
+++ test/test_merge_none_behavior_empty_dict.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import logging
+import platform
+
+basepath = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.dirname(basepath))
+import hiyapyco
+
+sys.path.insert(0, os.path.join(basepath, 'lib'))
+import testsetup
+
+logger = testsetup.setup(sys.argv[1:])
+
+print('start test %s for hiyapyco %s using python %s (loglevel:%s)' % (
+            __file__,
+            hiyapyco.__version__,
+            platform.python_version(),
+            logging.getLevelName(logger.getEffectiveLevel())
+        )
+    )
+
+
+logger.info("test none behavior EMPTY_DICT ...")
+conf = hiyapyco.load(
+    os.path.join(basepath, "base.yaml"),
+    os.path.join(basepath, "base_none_behavior_empty_dict.yaml"),
+    none_behavior=hiyapyco.NONE_BEHAVIOR_IGNORE,
+    method=hiyapyco.METHOD_MERGE,
+    failonmissingfiles=True,
+)
+
+t = conf["deepmap"]
+logger.info("test deepmap ... %s" % t)
+assert t == {
+    "l1k1": {"l2k1": "abc", "l2k2": "abcabc"},
+    "l1k2": {"l2k1": "bli", "l2k2": "bla", "l2k3": "blub"},
+}
+
+print("passed test %s" % __file__)
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent nu
